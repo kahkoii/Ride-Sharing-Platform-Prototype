2e2f8d6d55fcac1b8052e5edb11fde3a7af5c2d4
Kah Ho <sweekahho@gmail.com>
2021-11-11 23:29:29 +0800

    chore: remove default react files and add images and antd library

---
+++ client/src/PublicPage/App.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as Logo } from "../assets/ShrideLogo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
