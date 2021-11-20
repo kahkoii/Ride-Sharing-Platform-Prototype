ea9cf6dcb931e2812eececf4ad6d947dd4fcac98
Kah Ho <sweekahho@gmail.com>
2021-11-20 20:44:34 +0800

    feat: set routing logic for logging in for passengers and drivers

--- client/src/pages/App.tsx
+++ client/src/pages/App.tsx
@@ -1,31 +1,68 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { useState, useEffect } from "react";
 import PassengerPublic from "./Passenger/PassengerPublic";
 import DriverPublic from "./Driver/DriverPublic";
 import PassengerLogin from "./Passenger/PassengerLogin";
 import DriverLogin from "./Driver/DriverLogin";
+import MissingPage from "./MissingPage";
 
 const App: React.FC = () => {
   const [isPassengerLoggedIn, setIsPassengerLoggedIn] =
     useState<Boolean>(false);
+  const [isDriverLoggedIn, setIsDriverLoggedIn] = useState<Boolean>(false);
 
   useEffect(() => {
-    alert(isPassengerLoggedIn);
+    if (isPassengerLoggedIn) {
+      setIsDriverLoggedIn(false);
+      <Navigate to="/passenger" />;
+    }
   }, [isPassengerLoggedIn]);
+
+  useEffect(() => {
+    if (isDriverLoggedIn) {
+      setIsPassengerLoggedIn(false);
+      <Navigate to="/driver" />;
+    }
+  }, [isDriverLoggedIn]);
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<PassengerPublic />} />
-        <Route path="/for-drivers" element={<DriverPublic />} />
-        <Route
-          path="/passenger-login"
-          element={
-            <PassengerLogin setIsPassengerLoggedIn={setIsPassengerLoggedIn} />
-          }
-        />
-        <Route path="/driver-login" element={<DriverLogin />} />
-      </Routes>
+      {isPassengerLoggedIn && (
+        <Routes>
+          <Route
+            path="/passenger"
+            element={
+              <div style={{ backgroundColor: "white" }}>hi im passenger</div>
+            }
+          />
+          <Route path="*" element={<MissingPage />} />
+        </Routes>
+      )}
+      {isDriverLoggedIn && (
+        <Routes>
+          <Route path="/driver" element={<div>hi im passenger</div>} />
+          <Route path="*" element={<div>bitch what</div>} />
+        </Routes>
+      )}
+      {!isPassengerLoggedIn && !isDriverLoggedIn && (
+        <Routes>
+          <Route path="/" element={<PassengerPublic />} />
+          <Route path="for-drivers" element={<DriverPublic />} />
+          <Route path="driver-login" element={<DriverLogin />} />
+          <Route
+            path="passenger-login"
+            element={
+              <PassengerLogin setIsPassengerLoggedIn={setIsPassengerLoggedIn} />
+            }
+          />
+          <Route path="*" element={<MissingPage />} />
+        </Routes>
+      )}
     </Router>
   );
 };
