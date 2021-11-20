904eb57ea92ef98bf046244e592e0c9e67300aef
Kah Ho <sweekahho@gmail.com>
2021-11-20 22:26:15 +0800

    feat: create PassengerHome form

--- client/src/pages/Passenger/PassengerHome.tsx
+++ client/src/pages/Passenger/PassengerHome.tsx
@@ -1,4 +1,3 @@
-import { Link, useNavigate } from "react-router-dom";
 import MainNavbar from "./MainNavbar";
 import "../main.css";
 
@@ -7,13 +6,47 @@ interface passengerMainProps {
 }
 
 const PassengerMain = (props: passengerMainProps) => {
+  const findTrip = (event: any) => {
+    event.preventDefault();
+    console.log("Current location: " + event.target.locationPostal.value);
+    console.log("Want to go: " + event.target.destinationPostal.value);
+  };
+
   return (
     <>
       <MainNavbar setIsPassengerLoggedIn={props.setIsPassengerLoggedIn} />
       <div>
         <div className="image-overlay passenger-bg"></div>
         <div className="main">
-          <p>hello im passenger</p>
+          <div className="request-trip-section">
+            <div className="request-trip-inner">
+              <h1>Request Trip</h1>
+              <form className="login-form" onSubmit={findTrip}>
+                <h5 className="label">Location Postal Code</h5>
+                <input
+                  type="text"
+                  name="locationPostal"
+                  pattern="[0-9]{6}"
+                  placeholder="599489"
+                  required
+                />
+                <h5 className="label">Destination Postal Code</h5>
+                <input
+                  type="text"
+                  name="destinationPostal"
+                  pattern="[0-9]{6}"
+                  placeholder="599489"
+                  required
+                />
+                <button
+                  type="submit"
+                  className="request-btn public-register-btn passenger-btn"
+                >
+                  Find Trip
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </>
