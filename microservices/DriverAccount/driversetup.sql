fb418ea8922a46210808105071108d25b83d2740
Kah Ho <sweekahho@gmail.com>
2021-11-22 01:12:47 +0800

    feat: create driver_db setup sql file

---
+++ microservices/DriverAccount/driversetup.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE driver_db;
+
+USE driver_db;
+
+CREATE TABLE Drivers (
+	UID VARCHAR(16) NOT NULL PRIMARY KEY,
+    FirstName VARCHAR(30),
+    LastName VARCHAR(30),
+    Phone VARCHAR(8),
+    Email VARCHAR(50),
+    ID VARCHAR(9),
+    LicenseNo VARCHAR(8)
+);
+
+INSERT INTO Drivers (UID, FirstName, LastName, Phone, Email, ID, LicenseNo)
+VALUES ("l29skA2pXatUW5i7", "Lewis", "Hamilton", "82016712", "lham@hotmail.com", "T0209172L", "SGX9261R");
